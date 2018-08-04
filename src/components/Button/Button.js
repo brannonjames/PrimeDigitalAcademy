b235234fc97c7d2915fe5e851bedc80cddc2a315
Jimmy Brannon <user.email>
2018-08-03 23:10:03 -0500

    give Button option to pass children

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Button.css';
 
-export default ({ label, onClick, style }) => (
+export default ({ children, label, onClick, style }) => (
   <button
     onClick={onClick}
     className="Button"
     style={style}
   >
-    {label || 'Button'}
+    {children || label || 'Button'}
   </button>
-)
+);
